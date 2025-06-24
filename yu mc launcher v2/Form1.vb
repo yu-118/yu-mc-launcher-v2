aa47e3a06d56e2a8eb564957b5892509e812203e
yu <yu@DESKTOP-DVJ0BI2>
2025-06-24 16:18:33 +0900

    プロジェクト ファイルを追加します。

---
+++ yu mc launcher v2/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Text = My.Settings.ver
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Process.Start("JBOOTAPI.BAT", TextBox1.Text)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        My.Settings.ver = TextBox1.Text
+    End Sub
+End Class
